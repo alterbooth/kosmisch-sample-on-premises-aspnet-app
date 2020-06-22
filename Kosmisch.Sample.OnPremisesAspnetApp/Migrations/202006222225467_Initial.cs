16f2dd850dce51e7218434f9ba04e4c837a95bfd
h-mitsuzono <h-mitsuzono@alterbooth.com>
2020-06-23 07:29:24 +0900

    [add] Create database migration files

---
+++ Kosmisch.Sample.OnPremisesAspnetApp/Migrations/202006222225467_Initial.cs
@@ -0,0 +1,28 @@
+namespace Kosmisch.Sample.OnPremisesAspnetApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Name = c.String(),
+                        Age = c.Int(nullable: false),
+                        ProfileImageName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
---
+++ Kosmisch.Sample.OnPremisesAspnetApp/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Kosmisch.Sample.OnPremisesAspnetApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Kosmisch.Sample.OnPremisesAspnetApp.Data.MyContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Kosmisch.Sample.OnPremisesAspnetApp.Data.MyContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
