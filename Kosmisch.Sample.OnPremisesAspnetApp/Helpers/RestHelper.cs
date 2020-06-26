a997252d03c55eb2b954bc312b2ffa55aeb8f7b6
h-mitsuzono <h-mitsuzono@alterbooth.com>
2020-06-26 16:53:50 +0900

    [modify] RestHelper creating HttpClient object in method

--- Kosmisch.Sample.OnPremisesAspnetApp/Helpers/RestHelper.cs
+++ Kosmisch.Sample.OnPremisesAspnetApp/Helpers/RestHelper.cs
@@ -7,16 +7,20 @@ namespace Kosmisch.Sample.OnPremisesAspnetApp.Helpers
     /// </summary>
     public static class RestHelper
     {
-        static readonly HttpClient client = new HttpClient();
-
         /// <summary>
         /// GETリクエストを実行
         /// </summary>
         /// <returns>レスポンスbody</returns>
         public static string Get()
         {
-            var response = client.GetAsync("http://kosmischsample.net/").ConfigureAwait(false).GetAwaiter().GetResult();
-            var responseBody = response.Content.ReadAsStringAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+            var responseBody = "";
+
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync("http://kosmischsample.net/").ConfigureAwait(false).GetAwaiter().GetResult();
+                responseBody = response.Content.ReadAsStringAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+            }
+
             return responseBody;
         }
     }
