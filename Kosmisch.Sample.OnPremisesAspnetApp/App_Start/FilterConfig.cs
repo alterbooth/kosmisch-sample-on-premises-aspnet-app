3586c8d4824838acf2c957d7bd2a89cf61f88f70
h-mitsuzono <h-mitsuzono@alterbooth.com>
2020-06-27 16:24:08 +0900

    [add] LogFilter and support for logging of requests

--- Kosmisch.Sample.OnPremisesAspnetApp/App_Start/FilterConfig.cs
+++ Kosmisch.Sample.OnPremisesAspnetApp/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace Kosmisch.Sample.OnPremisesAspnetApp
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new Filters.LogFilter());
         }
     }
 }
---
+++ Kosmisch.Sample.OnPremisesAspnetApp/Filters/LogFilter.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace Kosmisch.Sample.OnPremisesAspnetApp.Filters
+{
+    public class LogFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var req = filterContext.HttpContext.Request;
+            Trace.WriteLine($"Action Executing.|Path={req.Path}");
+        }
+    }
+}
